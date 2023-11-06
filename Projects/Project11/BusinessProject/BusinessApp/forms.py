bd5efe079a9524d3f79e5d01c022b0c10236acb4
HashimThePassionate <hashiimtahir@gmail.com>
2023-11-06 11:14:01 +0500

    initial v3.3

---
+++ Projects/Project11/BusinessProject/BusinessApp/forms.py
@@ -0,0 +1,31 @@
+from django import forms
+from .models import Student
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+class contact(forms.ModelForm):
+    class Meta:
+        model = Student
+        fields = ['student_name','student_email','student_course']
+        labels = {'student_name':'Name','student_email':'Email','student_course':'Course'}
+        error_messages = {'student_name':{'required':'Please Enter Your Valid Name'},
+        'student_email':{'required':'Please Enter Your email with @'},
+        'student_course':{'required':'Please Enter Your Course Name'}
+        }
+        widgets= {
+            'student_name':forms.TextInput(attrs={'placeholder':'Enter Your Name'}),
+            'student_email':forms.EmailInput(attrs={'placeholder':'example@gmail.com'})
+        }
+        
+class customregistration(UserCreationForm):
+    password1=forms.CharField(min_length=8, max_length=60,widget=forms.PasswordInput(attrs={'class':'form-control','id':'password1'}),label="Password")
+    password2=forms.CharField(min_length=8, max_length=60,widget=forms.PasswordInput(attrs={'class':'form-control','id':'password2'}),label="Confirm Password") 
+
+    class Meta:
+        model = User
+        fields = ['username','email']
+        widgets= {
+            'username':forms.TextInput(attrs={'class':'form-control'}),
+            'email':forms.TextInput(attrs={'class':'form-control'}),
+            # 'first_name':forms.TextInput(attrs={'class':'form-control'}),
+            # 'last_name':forms.TextInput(attrs={'class':'form-control'}),
+        }
---
+++ Projects/Project11/BusinessProject/BusinessProject/urls.py
@@ -0,0 +1,37 @@
+"""
+URL configuration for BusinessProject project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from BusinessApp import views
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',views.home, name='home'),
+    path('about/',views.about, name='aboutus'),
+    path('product/',views.product, name='productus'),
+    path('store/',views.store, name='storeus'),
+    path('simple/',views.simpleForm, name='contactus'),
+    path('success/',views.success),
+    path('dynamic/<int:id>',views.dynamic, name="dynamic"),
+    path('student/',views.student,name="student"),
+    path('delete/<int:id>',views.delete ,name="delete"),
+    path('edit/<int:id>/',views.update ,name="update"),
+    path('signup/',views.reg ,name="signup"),
+    path('login/',views.login ,name="login"),
+    path('profile/',views.profile ,name="profile"),
+    path('logout/',views.logout_profile ,name="logout"),
+    path("accounts/", include("allauth.urls")),
+]
